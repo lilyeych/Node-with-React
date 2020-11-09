a47f268f2680fe467f7b9765f562ee4174aab199
Lily Han <lily.han@fcbsixconsulting.com>
2020-11-08 22:28:38 -0500

    65. Refactoring with Async/Await

--- services/passport.js
+++ services/passport.js
@@ -1,38 +1,42 @@
-const passport = require('passport');
-const mongoose = require('mongoose');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const keys = require('../config/keys');
+const passport = require("passport");
+const mongoose = require("mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("../config/keys");
 
-const User = mongoose.model('users');
+const User = mongoose.model("users");
 
 passport.serializeUser((user, done) => {
   done(null, user.id);
 });
 
 passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then(user => {
-      done(null, user);
-    })
+  User.findById(id).then((user) => {
+    done(null, user);
+  });
 });
 
-passport.use(new GoogleStrategy({
-  clientID: keys.googleClientID,
-  clientSecret: keys.googleClientSecret,
-  callbackURL: '/auth/google/callback',
-  proxy: true
-}, (accessToken, refreshToken, profile, done) => {
-  User.findOne({ googleId: profile.id })
-    .then((existingUser) => {
+passport.use(
+  new GoogleStrategy({
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback",
+      proxy: true,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({
+        googleId: profile.id
+      });
+      
       if (existingUser) {
         // we already have a record with the given profile ID
-        done(null, existingUser);
-      } else {
-        // we dont have a user record with this ID, make a new record
-        new User({ googleId: profile.id })
-          .save()
-          .then(user => done(null, user));
+        return done(null, existingUser);
       }
-    })
 
-}));
+      // we dont have a user record with this ID, make a new record
+      const user = await new User({
+        googleId: profile.id
+      }).save();
+      done(null, user);
+    }
+  )
+);
