affe006d8e3d8ec7df5d6ffc507f9f0251ad1f45
Lily Han <lily.han@fcbsixconsulting.com>
2021-01-20 21:29:53 -0500

    survey list and survey routes

--- client/src/components/surveys/SurveyList.js
+++ client/src/components/surveys/SurveyList.js
@@ -1,6 +1,6 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { fetchSurveys } from "../../actions";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { fetchSurveys } from '../../actions';
 
 class SurveyList extends Component {
   componentDidMount() {
@@ -8,14 +8,16 @@ class SurveyList extends Component {
   }
 
   renderSurveys() {
-    return this.props.surveys.reverse().map((survey) => {
+    return this.props.surveys.reverse().map(survey => {
       return (
         <div className="card darken-1" key={survey._id}>
           <div className="card-content">
             <span className="card-title">{survey.title}</span>
-            <p>{survey.body}</p>
+            <p>
+              {survey.body}
+            </p>
             <p className="right">
-              Send On: {new Date(survey.dateSent).toLocaleDateString()}
+              Sent On: {new Date(survey.dateSent).toLocaleDateString()}
             </p>
           </div>
           <div className="card-action">
@@ -26,8 +28,13 @@ class SurveyList extends Component {
       );
     });
   }
+
   render() {
-    return <div>{this.renderSurveys()}</div>;
+    return (
+      <div>
+        {this.renderSurveys()}
+      </div>
+    );
   }
 }
 
